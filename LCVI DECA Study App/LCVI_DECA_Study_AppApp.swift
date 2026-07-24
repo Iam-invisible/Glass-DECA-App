07e7a22b3f2c00ea2755de1ce4c23c52d0529145
Shail Patel <shailmp2009@gmail.com>
2026-07-23 22:05:16 -0400

    Initial Commit

---
+++ LCVI DECA Study App/LCVI_DECA_Study_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCVI_DECA_Study_AppApp.swift
+//  LCVI DECA Study App
+//
+//  Created by Shail Patel on 2026-07-23.
+//
+
+import SwiftUI
+
+@main
+struct LCVI_DECA_Study_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
